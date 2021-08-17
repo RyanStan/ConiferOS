bee07eba5f84f4e5ab95a0da4a458746c4043a36
Ryan Stankiewicz <ryanstan@vt.edu>
2021-08-17 12:31:41 +0000

    added paging example and test in kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -17,7 +17,21 @@ void kernel_main()
 
 	paging = init_page_tables(PAGING_READ_WRITE | PAGING_PRESENT | PAGING_USER_SUPERVISOR);
 	paging_switch(get_pgd(paging));
+
+	/* Example to make sure paging works 
+	 * vaddr 0x1000 will point to the allocated memory at ptr
+	 */
+	char *ptr = kzalloc(PAGING_PAGE_SIZE);
+	paging_set(get_pgd(paging), (void*)0x1000, (uint32_t)ptr | PAGING_USER_SUPERVISOR | PAGING_PRESENT | PAGING_READ_WRITE);
+
 	enable_paging();
+
+	char *ptr2 = (char*)0x1000;
+	ptr2[0] = 'A';
+	ptr2[1] = 'B';
+	/* Should see ABAB printed */
+	print(ptr2);
+	print(ptr);
 
 	enable_interrupts();
 
