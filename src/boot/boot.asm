e4614f89b96c25889f1960f17a22bc624935d7f5
Ryan Stankiewicz <ryanstan@vt.edu>
2021-08-18 13:08:30 +0000

    clean up ata comments in bootloader

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,88 +1,88 @@
 ; Intel syntax
 ; I know, there are a lot of comments.  I'm a newbie to Intel syntax and x86 assembly 
 
-ORG 0x7c00		; This is where the program expects to be loaded into memory
-BITS 16			; Tells the assembler we are using a 16 bit architecture
+ORG 0x7c00			; This is where the program expects to be loaded into memory
+BITS 16				; Tells the assembler we are using a 16 bit architecture
 
 CODE_SEG equ gdt_code - gdt_start	; EQU is a NASM psuedo instruction that gives a symbol (CODE_SEG) a corresponding value (gdt_code - gdt_start)
 DATA_SEG equ gdt_data - gdt_start	; These symbols will be used to give us our offsets into the GDT for the respective segment descriptors
 
-_start:			; the following instructions signify the start of the boot record (look at wiki.osdev.org/FAT)
+_start:				; the following instructions signify the start of the boot record (look at wiki.osdev.org/FAT)
 	jmp short start
 	nop
 
-times 33 db 0		; creates 33 bytes after previous nop instruction to fill our boot record with arbitrary values
+times 33 db 0			; creates 33 bytes after previous nop instruction to fill our boot record with arbitrary values
 
 start:
-	jmp 0x0:step2	; sets the code segment register to 0x0.  This will actually be address 0x7c00 since we set ORG 0x7c00
+	jmp 0x0:step2		; sets the code segment register to 0x0.  This will actually be address 0x7c00 since we set ORG 0x7c00
 
 step2:
-	cli		; clear interrupts (so that they're disabled while we change segment registers)
+	cli			; clear interrupts (so that they're disabled while we change segment registers)
 	mov ax, 0x0
-	mov ds, ax	; setup data segment.  note that data segment will start at 0x7c0 * 16 + 0, or 0x7c00, which is the location that BIOS jumps to for execution of bootloader
-	mov es, ax	; setup extra segment
+	mov ds, ax		; setup data segment.  note that data segment will start at 0x7c0 * 16 + 0, or 0x7c00, which is the location that BIOS jumps to for execution of bootloader
+	mov es, ax		; setup extra segment
 	mov ss, ax
-	mov sp, 0x7c00 	; remember from before: 0x7c00 is the address that RAM will jump to execute bootloader.  ss * 16 + sp = 0x7c00
-	sti		; enables interrupts
+	mov sp, 0x7c00 		; remember from before: 0x7c00 is the address that RAM will jump to execute bootloader.  ss * 16 + sp = 0x7c00
+	sti			; enables interrupts
 
-	mov si, message ; si stands for source index. 16 bit low end of esi register
+	mov si, message 	; si stands for source index. 16 bit low end of esi register
 	call print
 
 
-.load_protected:	; load the processor into protected mode
+.load_protected:		; load the processor into protected mode
 	cli
 	lgdt[gdt_descriptor]
 	mov eax, cr0
-	or al, 0x1	; set PE (Protection Enable) bit in CR0 (control register 0)
+	or al, 0x1		; set PE (Protection Enable) bit in CR0 (control register 0)
 	mov cr0, eax
-	jmp CODE_SEG:load32 ; sets the code segment selector to CODE_SEG and jumps to load32
+	jmp CODE_SEG:load32 	; sets the code segment selector to CODE_SEG and jumps to load32
 
 ; GDT
 gdt_start:
 gdt_null:
-	dd 0x0 		; 4 bytes of 0s
-	dd 0x0 		; This will represent a null entry (null segment descriptor) in our gdt
+	dd 0x0 			; 4 bytes of 0s
+	dd 0x0 			; This will represent a null entry (null segment descriptor) in our gdt
 
 ; offset 0x8 - this is the offset in our GDT where we will make the code segment descriptor entry (will be the 2nd entry in our gdt since each entry is 8 bytes)
-gdt_code:		; CS SHOULD POINT TO THIS
-	dw 0xffff 	; Segment Limit 0:15 bits.  This is the maximum addressable unit for this segment (page granularity, 4 KiB in this case)
-	dw 0		; Base 0:15.  The base describes the linear address where the segment begins
-	db 0		; Base 16:23 bits
-	db 0x9a		; Access byte
-	db 11001111b	; High 4 bit flags and low 4 bit flags
-	db 0		; Base 24-31 bits
+gdt_code:			; CS SHOULD POINT TO THIS
+	dw 0xffff 		; Segment Limit 0:15 bits.  This is the maximum addressable unit for this segment (page granularity, 4 KiB in this case)
+	dw 0			; Base 0:15.  The base describes the linear address where the segment begins
+	db 0			; Base 16:23 bits
+	db 0x9a			; Access byte
+	db 11001111b		; High 4 bit flags and low 4 bit flags
+	db 0			; Base 24-31 bits
 
-; offset 0x10		; (3rd entry in our gdt - we're actually using this for DS, SS, ES, FS, and GS since we're not really using segmentation anyways)
-gdt_data:		; DS, SS, ES, FS, GS
-	dw 0xffff 	; Segment Limit 0:15 bits.  This is the maximum addressable unit for this segment (page granularity, 4 KiB in this case)
-	dw 0		; Base 0:15.  The base describes the linear address where the segment begins
-	db 0		; Base 16:23 bits
-	db 0x92		; Access byte
-	db 11001111b	; High 4 bit flags and low 4 bit flags
-	db 0		; Base 24-31 bits
+; offset 0x10			; (3rd entry in our gdt - we're actually using this for DS, SS, ES, FS, and GS since we're not really using segmentation anyways)
+gdt_data:			; DS, SS, ES, FS, GS
+	dw 0xffff 		; Segment Limit 0:15 bits.  This is the maximum addressable unit for this segment (page granularity, 4 KiB in this case)
+	dw 0			; Base 0:15.  The base describes the linear address where the segment begins
+	db 0			; Base 16:23 bits
+	db 0x92			; Access byte
+	db 11001111b		; High 4 bit flags and low 4 bit flags
+	db 0			; Base 24-31 bits
 
 gdt_end:
 
-gdt_descriptor:		; This is the GDT description structure that will be used by the lgdt instruction
+gdt_descriptor:			; This is the GDT description structure that will be used by the lgdt instruction
 	dw gdt_end - gdt_start - 1	; Size
-	dd gdt_start	; Offset - linear address of the GDT itself
+	dd gdt_start		; Offset - linear address of the GDT itself
 
 
 print:
-	mov bx, 0	; part of interrupt call
+	mov bx, 0		; part of interrupt call
 .loop:
-	lodsb		; loads the character that si register is pointing to into al register.  then increments si register so it points to next char
+	lodsb			; loads the character that si register is pointing to into al register.  then increments si register so it points to next char
 	cmp al, 0
 	je .done
 	call print_char
 	jmp .loop
 
-.done:			; nasm sub label
+.done:				; nasm sub label
 	ret
 
 print_char:
-	mov ah, 0eh	; function number = 0Eh : Display character
-	int 0x10        ; call INT 10h (0x10), BIOS video service 
+	mov ah, 0eh		; function number = 0Eh : Display character
+	int 0x10        	; call INT 10h (0x10), BIOS video service 
 	ret
 
 
@@ -101,7 +101,7 @@ load32:
 
 ata_lba_read:
 	; ata/ide is a protocol for communicating with disk drives attached via the motherboard to CPU
-	mov ebx, eax ; backup the logical block address (lba) 
+	mov ebx, eax 		; backup the logical block address (lba) 
 	; send the highest 8 bits of the lba to disk controller
 	shr eax, 24		; shift the eax register 24 bits to the right so that eax contains highest 8 bits of the lba (shr does not wrap)
 	or eax, 0xE0		; selects the master drive
@@ -112,53 +112,48 @@ ata_lba_read:
 
 	; send the total number of sectors that we are reading to the hard disk controller
 	mov eax, ecx
-	mov dx, 0x01F2
+	mov dx, 0x01F2		; 0x01F2 --> set sectorcount
 	out dx, al
 	; finished sending the total sectors to read
 	
-	; send more bits of the lba
+	; send low bits of the lba
 	mov eax, ebx		; restore the backup lba
-	mov dx, 0x01F3
+	mov dx, 0x01F3		; 0x01F3 --> set LBAlo
 	out dx, al
-	; finished sending more bits of the lba
 
-	; Send more bits of the lba
-	mov dx, 0x01F4
-	mov eax, ebx 		; restore the backup lba (do this each time we edit it to be safe)
+	; Send mid bits of the lba
+	mov dx, 0x01F4		; 0x01F4 --> LBAmid
+	mov eax, ebx 		
 	shr eax, 8
 	out dx, al
-	; Finished sending more bits of the lba
 
 	; Send upper 16 bits of the lba
-	mov dx, 0x1F5
-	mov eax, ebx		; restore the backup lba
+	mov dx, 0x1F5		; 0x01F5 --> LBAhi
+	mov eax, ebx		
 	shr eax, 16 
 	out dx, al
-	; Finished sending upper 16 bits of the LBA
 
-	mov dx, 0x01F7
-	mov al, 0x20
+	mov dx, 0x01F7		; 0x01F7 --> Command IO Port
+	mov al, 0x20		; 0x0020 --> Read sector(s) command
 	out dx, al
 
 	; Read all sectors into memory
 .next_sector:
-	push ecx	; push ecx onto the stack to save for later (remember it has the total # of sectors we want to read)
+	push ecx		; push ecx onto the stack to save for later (remember it has the total # of sectors we want to read)
 
 ; checking if we need to read
 .try_again:
 	mov dx, 0x01F7	
-	in al, dx	; we read into al from the port specified by dx
-	test al, 8	; check to see if this bit is set in al
-	jz .try_again	; jumps back to try_again if test fails
+	in al, dx		; we read the ATA status register into al by reading from the command io port
+	test al, 8		; check to see if the busy bit is set.  If set, the disk drive still has control of the command block registers (still reading)
+	jz .try_again		; jumps back to try_again if the busy bit is set
 
-; we need to read 256 words (512 bytes, or one sector) at a time
+	; we need to read 256 words (512 bytes, or one sector) at a time
  	mov ecx, 256
 	mov dx, 0x01F0
-	rep insw	; reads word from I/O port specified by dx into memory location specified by ES:(E)DI
-			; rep insw reads a word from port 0x01F0 and stores it into 0x0100000 (1 MB, specified by edi)
-	pop ecx		; restores the ecx that we saved via push earlier 
+	rep insw		; rep insw reads a word from port 0x01F0 and stores it into 0x0100000 (1 MB, specified by edi register)
+	pop ecx			; restores the ecx that we saved via push earlier (contains count of sectors left to read
 	loop .next_sector	; decrements ecx register (contains # registers to read) until it hits 0 and we've read all sectors
-	; End of reading sectors from disk into memory
 	ret
 	
 
