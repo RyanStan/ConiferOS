310e3242eb7bb49749d1276e6b8419ca19104680
Ryan Stankiewicz <ryanstan@vt.edu>
2021-08-30 12:21:31 +0000

    more string functions

--- src/string/string.c
+++ src/string/string.c
@@ -10,3 +10,16 @@ int strlen(const char *ptr)
 
         return i;
 }
+
+int ctoi(char c) 
+{
+        if (is_digit(c))
+                return c - 48;
+        else
+                return -1;
+}
+
+bool is_digit(char c)
+{
+        return (int)c > 47 && (int)c < 58;
+}
--- src/string/string.h
+++ src/string/string.h
@@ -1,6 +1,18 @@
 #ifndef STRING_H
 #define STRING_H
 
+#include <stdbool.h>
+
+/* Returns the length of the null terminated string at ptr */
 int strlen(const char *ptr);
+
+/* Converts the character argument c to an integer 
+ * Expects c to be a character from 0-9.  Returns -1 if not true
+ */
+int ctoi(char c);
+
+/* Returns true if the character c is a digit between '0' and '9' */
+bool is_digit(char c);
+
 
 #endif
