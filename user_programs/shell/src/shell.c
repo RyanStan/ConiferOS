632c645a0a2ad660f7816b5d003cb48d127dccf1
RyanStan <ryan.stankiewicz0@gmail.com>
2023-11-05 19:35:17 -0500

    User shell: parse commands into tokens
    Implemented the parse_command function in the user shell, which will be
    used to parse commands into individual tokens.

--- user_programs/shell/src/shell.c
+++ user_programs/shell/src/shell.c
@@ -2,6 +2,10 @@
 #include "stdio.h"
 #include "coniferos.h"
 #include "string.h"
+#include "stdlib.h"
+#include "memory.h"
+
+#define MAX_COMMAND_LEN 1024
 
 int main(int argc, char **argv) 
 {
@@ -18,4 +22,47 @@ int main(int argc, char **argv)
         coniferos_exec(path_buf);
     }
     return 0;
+}
+
+struct command_token *parse_command(const char *command, int command_len)
+{
+    struct command_token *root_command = 0;
+    if (command_len > MAX_COMMAND_LEN) {
+        return 0;
+    }
+
+    char token_buf[1024];
+    char *token = strtok(command, ' ', token_buf);
+    if (!token) {
+        return 0;
+    }
+
+    root_command = malloc(sizeof(struct command_token));
+    if (!root_command) {
+        return 0;
+    }
+
+    strncpy(root_command->token, token, sizeof(root_command->token));
+    root_command->next = 0;
+
+    
+    struct command_token *current = root_command;
+    memset(token_buf, 0, sizeof(token_buf));
+    token = strtok(NULL, ' ', token_buf);
+    while (token != NULL) {
+        struct command_token *next_arg = malloc(sizeof(struct command_token));
+        if (!next_arg) {
+            free(root_command);
+            return 0;
+        }
+
+        strncpy(next_arg->token, token, sizeof(root_command->token));
+        next_arg->next = 0;
+        current->next = next_arg;
+        current = next_arg;
+        memset(token_buf, 0, sizeof(token_buf));
+        token = strtok(NULL, ' ', token_buf);
+    }
+
+    return root_command;
 }
--- user_programs/shell/src/shell.h
+++ user_programs/shell/src/shell.h
@@ -1,4 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define MAX_CMMD_ARG_LEN 512
+
+struct command_token {
+    char token[MAX_CMMD_ARG_LEN];
+    struct command_token *next;
+};
+
+// Split a command string into a linked list of command tokens
+struct command_token *parse_command(const char *command, int command_len);
+
 #endif
